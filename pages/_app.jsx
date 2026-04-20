30f733bf99a8bc60bdbb8e24c874fb9c095673e2
keke <keke>
2026-04-19 22:36:28 -0700

    feat: add GA

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,24 @@
 import '../styles/globals.css';
+import Script from 'next/script';
+
+const GA_MEASUREMENT_ID = 'G-RE3519EZ3K';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
